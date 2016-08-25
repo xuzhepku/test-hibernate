2b0c0e338defa872f3d1440ffbd1fc471e497e01
JasonHsu <xuzhepku@gmail.com>
2016-08-25 23:26:05 +0800

    update and delete user.

--- src/test/java/me/xuzhe/AppTest01.java
+++ src/test/java/me/xuzhe/AppTest01.java
@@ -21,14 +21,14 @@ public class AppTest01 {
             session = HibernateUtil.openSession();
             session.beginTransaction();
 
-
+            // 真正业务逻辑
             User u = new User();
             u.setBorn(sdf.parse("1982-10-30"));
             u.setNickname("jiji");
             u.setUsername("xuzhe");
             u.setPassword("123");
             session.save(u);
-
+            // end of 真正业务逻辑
             session.getTransaction().commit();
         } catch (Exception e) {
             e.printStackTrace();
@@ -47,6 +47,50 @@ public class AppTest01 {
             System.out.println(u);
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            HibernateUtil.closeSession(session);
+        }
+    }
+
+    @Test
+    public void testUpdate() {
+        Session session = null;
+        try {
+            session = HibernateUtil.openSession();
+            session.beginTransaction();
+
+            // 真正业务逻辑
+            User u = session.load(User.class, 1);
+            u.setNickname("new nick name");
+            session.save(u);
+            // end of 真正业务逻辑
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (session != null) session.getTransaction().rollback();
+        } finally {
+            HibernateUtil.closeSession(session);
+        }
+    }
+
+    @Test
+    public void testDelte() {
+        Session session = null;
+        try {
+            session = HibernateUtil.openSession();
+            session.beginTransaction();
+
+            // 真正业务逻辑
+            User u = new User();
+            u.setId(1);
+            session.delete(u);
+            // end of 真正业务逻辑
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (session != null) session.getTransaction().rollback();
         } finally {
             HibernateUtil.closeSession(session);
         }
